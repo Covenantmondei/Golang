8ce5c31c6f1f8937c0f80a11838968f67323b6ba
Covenant Monday <covenantmonday863@gmail.com>
2026-02-09 15:54:36 +0100

    added main

---
+++ test.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	// fmt.Println("Hello Covenant! Welcome to Golang")
+
+	var NameOne string = "Covenant"
+	var NameTwo = "Monday"
+	var NameThree string
+
+
+	fmt.Println(NameOne, NameTwo, NameThree)
+
+	NameOne = "Governor"
+
+	NameThree = "Browser"
+	fmt.Println(NameOne, NameTwo, NameThree)
+
+	var ageOne int = 40
+	var ageTwo = 30
+
+	ageThree := 20
+
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	// Bits and memory
+
+
+	// Float
+	var decimalOne float32 = 5.5
+	var decimalTwo float64 = 58.912
+	fmt.Println(decimalOne, decimalTwo)
+}
